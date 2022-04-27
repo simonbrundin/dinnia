0aa0d8e6b8017783677b9b3281beefa7caf9f002
Simon Brundin <simonbrundin@gmail.com>
2022-04-27 22:13:50 +0200

    moved css from global to component

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,11 +1,41 @@
 <template>
-  <div class="fullscreen">
-    <div class="card"><slot></slot></div>
+  <div class="modal-backdrop" @click.self="close">
+    <div class="modal">
+      <div class="card"><slot></slot></div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    close() {
+      this.$emit('close')
+      console.log('hej')
+    },
+  },
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+.modal-backdrop {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 100;
+}
+
+.modal {
+  background: #ffffff;
+  box-shadow: 2px 2px 20px 1px;
+  overflow-x: auto;
+  display: flex;
+  flex-direction: column;
+}
+</style>
