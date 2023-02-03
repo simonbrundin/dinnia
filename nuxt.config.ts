2edaae739f41fa4285dbf2ea790624466616b01f
Simon Brundin <simonbrundin@gmail.com>
2023-02-03 09:11:01 +0100

    new installation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,45 +1,4 @@
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['~/assets/css/tailwind.css'],
-  modules: [
-    // 'nuxt-graphql-client',
-    '@nuxtjs/tailwindcss',
-    '@pinia/nuxt',
-    'nuxt-icons',
-    '@nuxtjs/apollo',
-  ],
-  apollo: {
-    clients: {
-      default: {
-        httpEndpoint: 'https://matkasse.hasura.app/v1/graphql',
-        tokenName: 'x-hasura-admin-secret',
-      },
-    },
-  },
-  runtimeConfig: {
-    public: {
-      'graphql-client': {
-        clients: {
-          default: {
-            host: 'https://matkasse.hasura.app/v1/graphql',
-            token: {
-              name: 'x-hasura-admin-secret',
-              type: null,
-            },
-            retainToken: true,
-          },
-          // willys: {
-          //   host: 'https://countries.trevorblades.com/graphql', // process.env.GQL_COUNTRIES_HOST
-          //   token: {
-          //     name: 'X-Custom-Auth', // process.env.GQL_COUNTRIES_TOKEN_NAME
-          //     value: 'your_access_token', // process.env.GQL_COUNTRIES_TOKEN
-          //   },
-          // },
-        },
-      },
-    },
 
-    // plugins: ['~/plugins/auth0.js'],
-    // middleware: ['~/middleware/auth.global.ts'],
-  },
 })
