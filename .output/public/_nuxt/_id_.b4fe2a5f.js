d9af80bec2d3479113302bb4ed5e579208d2425f
Simon Brundin <simonbrundin@gmail.com>
2023-03-29 08:42:15 +0200

    changes

---
+++ .output/public/_nuxt/_id_.b4fe2a5f.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper.c27b6911.js";import{o,h as r}from"./entry.ae526870.js";const c={};function t(n,_){return o(),r("div",null,"yes")}const f=e(c,[["render",t]]);export{f as default};
---
+++ .output/public/_nuxt/auth.32d75be0.js
@@ -0,0 +1,1 @@
+import{z as t}from"./entry.ae526870.js";const r=t({id:"auth-store",state:()=>({signInErrorMessage:""}),actions:{},getters:{}});export{r as u};
